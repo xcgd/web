c161a933b23250cef298d346242326c09a93ca83
Vincent Hatakeyama <vincent.hatakeyama@xcg-consulting.fr>
2019-05-22 16:23:49 +0200

    ✨ add option to set a label for total column

--- web_widget_x2many_2d_matrix/static/src/js/widget_x2many_2d_matrix.js
+++ web_widget_x2many_2d_matrix/static/src/js/widget_x2many_2d_matrix.js
@@ -67,6 +67,7 @@ odoo.define("web_widget_x2many_2d_matrix.widget", function (require) {
                 node.show_column_totals ||
                     this.is_aggregatable(field_defs[this.field_value])
             );
+            this.label_row_totals = node.label_row_totals || "";
         },
 
         /**
@@ -128,6 +129,7 @@ odoo.define("web_widget_x2many_2d_matrix.widget", function (require) {
                 columns: this.columns,
                 rows: this.rows,
                 show_row_totals: this.show_row_totals,
+                label_row_totals: this.label_row_totals,
                 show_column_totals: this.show_column_totals,
             };
         },
