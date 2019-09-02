4cdac5fa689b9b1533d097a2d6f7820c9ef80475
BALDE <tansadio@gmail.com>
2019-09-02 14:34:16 +0200

    [MIG] web_widget_one2many_tags: migration to 11.0

--- web_widget_one2many_tags/__manifest__.py
+++ web_widget_one2many_tags/__manifest__.py
@@ -1,9 +1,8 @@
-# -*- coding: utf-8 -*-
 # © 2016 Therp BV <http://therp.nl>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Tags widget for one2many fields",
-    "version": "8.0.1.0.0",
+    "version": "11.0.1.0.0",
     "author": "Therp BV,Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "category": "Hidden/Dependency",
@@ -13,9 +12,7 @@
         'web',
     ],
     "data": [
-        'views/templates.xml',
+        'views/assets.xml',
     ],
-    "qweb": [
-        'static/src/xml/web_widget_one2many_tags.xml',
-    ],
+    "qweb": ["static/src/xml/*.xml"],
 }
